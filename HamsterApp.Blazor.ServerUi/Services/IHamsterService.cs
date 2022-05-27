cf22ce34f9de19afc76ee90efd7847dc4f6c6cef
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-05-27 14:59:38 -0700

    Hamster for display for /battle/

--- HamsterApp.Blazor.ServerUi/Services/IHamsterService.cs
+++ HamsterApp.Blazor.ServerUi/Services/IHamsterService.cs
@@ -5,6 +5,7 @@ namespace HamsterApp.Blazor.ServerUi.Services
     public interface IHamsterService
     {
         Task<Response<List<HamsterReadOnlyDto>>> GetHamsters();
+        Task<Response<List<HamsterDetailsDto>>> GetTwoHamsters();
         Task<Response<HamsterReadOnlyDto>> GetHamster(int id);
         Task<Response<HamsterUpdateDto>> GetHamsterForUpdate(int id);
         Task<Response<int>> CreateHamster(HamsterCreateDto hamster);
